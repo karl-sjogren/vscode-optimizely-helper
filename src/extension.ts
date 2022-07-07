8bf16890d469892a086db31a779a99a793d93fc8
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2022-07-07 21:07:26 +0200

    Removed some boilerplate and added some more fun stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { service } from './optimizely-service';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "karls-optimizely-helpers" is now active!');
+	checkForTemplates();
+	checkForConfigFile();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('karls-optimizely-helpers.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Karls Optimizely Helpers!');
+	let disposable = vscode.commands.registerCommand('karls-optimizely-helpers.createContentType', async () => {
+		const name = await vscode.window.showInputBox({
+			prompt: "Content type name: ",
+			placeHolder: "StartPage"
+		});
+
+		if(name?.trim().length === 0) {
+			return;
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+async function checkForTemplates() {
+	const templatesInstalled = await service.checkForTemplates();
+		
+	if(templatesInstalled) {
+		return;
+	}
+
+	const instructionsAction = 'Read instructions';
+	const installAction = 'Try installing templates';
+
+	const selectedAction = await vscode.window.showInformationMessage(
+		'Episerver Templates not available.',
+		instructionsAction,
+		installAction,
+	);
+
+	if(selectedAction === instructionsAction) {
+		vscode.env.openExternal(vscode.Uri.parse('https://github.com/karl-sjogren/vscode-optimizely-helper/'));
+	}
+
+	if(selectedAction === installAction) {
+		const installSuccess = await service.tryInstallTemplates();
+		vscode.window.showInformationMessage(
+			installSuccess ? 'Episerver templates installed.' : 'Failed to install Episerver templates.',
+		);
+	}
+}
+
+async function checkForConfigFile() {
+	const configExists = await service.checkIfConfigExists();
+	if(configExists !== 'ConfigMissing') {
+		return;
+	}
+
+	const createConfigAction = 'Create .optimizelyrc';
+
+	const selectedAction = await vscode.window.showInformationMessage(
+		'No workspace configuration found.',
+		createConfigAction,
+	);
+
+	if(selectedAction === createConfigAction) {
+		await service.createConfig(true);
+	}
 }
 
 // this method is called when your extension is deactivated
